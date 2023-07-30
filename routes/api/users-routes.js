d36e8ad8bfbd24a68294a8abf2480279db015b65
dbey21 <125637145+dbey21@users.noreply.github.com>
2023-07-30 00:43:42 -0700

    Social Network API

---
+++ routes/api/users-routes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/User-contollers");
+
+router.route("/").get(getAllUser).post(createUser);
+
+// /api/users/:id
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
+module.exports = router;
